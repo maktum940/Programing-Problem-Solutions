8a16b34545be57440085931560e30c821f4515c3
Masrurul Hasan <mhmaktum006@gmail.com>
2021-07-15 18:29:13 +0600

    ^ Operator

---
+++ URI Online Judge/1026 (To Carry or not to Carry).cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    unsigned long int a, b;
+
+    while(cin >> a >> b){
+
+        unsigned int res = a ^ b;
+
+        cout << res << endl;
+    }
+}
