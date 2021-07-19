53f738e5fb56e7d3823e4169be9e6ae1387b88fc
Masrurul Hasan <mhmaktum006@gmail.com>
2021-07-19 16:22:01 +0600

    Easy Math Problem

---
+++ Codeforces/1550 A (Find The Array).cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(){
+    int n, res;
+    cin >> n;
+
+    res = ceil(sqrt(n));
+
+    cout << res << endl;
+}
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        solve();
+    }
+}
