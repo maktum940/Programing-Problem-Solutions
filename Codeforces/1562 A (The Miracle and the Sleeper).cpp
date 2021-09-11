6182f5cf5a383ff56ea94a4ff66bd791b5b1998a
Masrurul Hasan <mhmaktum006@gmail.com>
2021-09-11 11:56:26 +0600

    Most Higher Reminder

---
+++ Codeforces/1562 A (The Miracle and the Sleeper).cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(){
+    int l, r;
+    cin >> l >> r;
+
+    if(r < l*2) {
+        cout << r-l << endl;
+    }
+    else{
+        cout << (r-1)/2 << endl;
+    }
+}
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        solve();
+    }
+}
