72c8df07201c23f924aa1a33faca091592650565
maktum940 <64151360+maktum940@users.noreply.github.com>
2023-12-06 14:41:54 +0600

    1907_B

--- Codeforces/1341 A (Nastya and Rice).cpp
+++ Codeforces/1341 A (Nastya and Rice).cpp
@@ -18,3 +18,52 @@ int main()
     }
     return 0;
 }
+
+///////////...1907...B..//////
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+void solve(){
+    string s;
+    string res;
+    cin >> s;
+    
+    int len = s.size();
+    
+    for(int i = 0; i = s.size();i++){
+        if(s[i] == 'b'){
+            for(int j = len-1; j > 0; j--){
+                if(123 > res[j] && res[j] > 96){
+                    s.erase(s.begin() + j);
+                    break;
+                }
+            }
+        }
+        else if(s[i] == 'B'){
+            for(int j = len-1; j > 0; j--){
+                if(91 > res[j] && res[j] > 64){
+                    s.erase(s.begin() + j);
+                    break;
+                }
+            }
+        }
+        else{
+            res = res+s[i];
+        }
+    }
+    cout << len << endl;
+}
+
+int main() {
+    int t;
+    
+    cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
+
