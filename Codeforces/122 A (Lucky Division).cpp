026c8818a3fe996f423140de7bf35b067f1dbbb1
maktum940 <64151360+maktum940@users.noreply.github.com>
2023-12-14 18:44:27 +0600

    none

--- Codeforces/122 A (Lucky Division).cpp
+++ Codeforces/122 A (Lucky Division).cpp
@@ -11,39 +11,3 @@ int main()
     }
     return 0;
 }
-
-/////////////////...1907...A...
-#include <bits/stdc++.h>
-
-using namespace std;
-
-void solve(){
-    string s;
-    cin >> s;
-    
-    int row = s[1]-'0';
-    char column = s[0];
-    
-    for(int i = 1; i <=8; i++){
-        if(i == row) continue;
-        
-        char res[2] = {s[0], i+'0'};
-        cout << res[0] << res[1] << endl;
-    }
-    for(int i = 0; i < 8; i++){
-        char a = 'a'+i;
-        if(a == column) continue;
-        
-        cout << a << 5 << endl;
-    }
-}
-
-int main() {
-    int t;
-    cin >> t;
-    while(t--){
-        solve();
-    }
-
-    return 0;
-}
